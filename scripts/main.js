ad71f09b3f58ea567b105293691f7839008a059d
LeraB <kudravka13@gmail.com>
2018-01-24 23:47:12 +0200

    fix bugs

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,26 @@
-var slideIndex = 0;
-showSlides();
+var slideIndex = 0,
+    start = 0;
+showRightSlides();
 
-function showSlides(e) {
+function showLeftSlides(e) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = slides.length-1; i >=0; i--) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    for (i = dots.length-1; i >=0; i--) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex + 1].style.display = "block";
+    dots[slideIndex + 1].className += " active";
+}
+
+function showRightSlides(e) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
@@ -20,12 +39,19 @@ function showSlides(e) {
 }
 
 document.getElementById("slideshow-container").addEventListener("click", function (e) {
-    showSlides(e);
+    if(event.clientX -event.offsetX > 0){
+    showRightSlides(e);}
+    else
+    {
+    if(event.clientX -event.offsetX < 0)  {
+        showLeftSlides(e);
+    }  
+    }
 });
 
 document.getElementById("dots-wraper").addEventListener("click", function (e) {
-    showSlides(e);
+    showRightSlides(e);
 });
 document.getElementById("dots-wraper").addEventListener("click", function (e) {
-    showSlides(e);
+    showRightSlides(e);
 });
